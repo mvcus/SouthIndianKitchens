333e535a1b8b4a973387f124b78695b0d15dc978
Chandan31g <chandangupta8702@gmail.com>
2020-05-11 21:38:53 +0530

    Commit included code fix.

--- SouthIndianKitchens.APP/ClientApp/src/app/app.module.ts
+++ SouthIndianKitchens.APP/ClientApp/src/app/app.module.ts
@@ -1,12 +1,13 @@
-/// <reference path="recpie/filterrecpie.pipe.ts" />
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AuthService } from './_services/auth.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
-import {FormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { UploadComponent } from './Upload/Upload.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { AdminComponent } from './admin/admin.component';
 import { ListsComponent } from './lists/lists.component';
@@ -15,17 +16,19 @@ import { appRoutes } from './routes';
 import { RegisterComponent } from './register/register.component';
 import { EpisodeComponent } from './episode/episode.component';
 import { SafeUrlPipe } from './safe-url.pipe';
-import { UploadImageService } from './_services/upload-image.service';
-import { UploadComponent } from './Upload/Upload.component';
+import { ImageSliderComponent } from './imageslider/imageslider.component';
+import { NgImageSliderModule } from 'ng-image-slider';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 import { ImageDetailComponent } from './recpie/Recpie-Details.component';
 import { FilterimagesPipe } from './recpie/filterrecpie.pipe';
 import { RecpieComponent } from './recpie/recpie.component';
 import { ImageService } from './recpie/image.service';
 import { ReviewsComponent } from './reviews/reviews.compnent';
 import { AboutusComponent } from './aboutus/aboutus.component';
+import { UploadImageService } from './_services/upload-image.service';
 
 @NgModule({
-   declarations: [
+  declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
@@ -35,28 +38,29 @@ import { AboutusComponent } from './aboutus/aboutus.component';
     ListsComponent,
     RegisterComponent,
     EpisodeComponent,
+    SafeUrlPipe,
+    ImageSliderComponent,
     RecpieComponent,
     ReviewsComponent,
     AboutusComponent,
     ImageDetailComponent,
     FilterimagesPipe,
-    SafeUrlPipe
-   ],
-   imports: [
+  ],
+  imports: [
+    BrowserAnimationsModule,
     BrowserModule,
-     HttpClientModule,
-     
+    HttpClientModule,
+    TabsModule.forRoot(),
     FormsModule,
-     RouterModule.forRoot(appRoutes),
-     
-   ],
-   providers: [
-     AuthService,
-     UploadImageService,
-     ImageService
-   ],
-   bootstrap: [
-      AppComponent
-   ]
+    RouterModule.forRoot(appRoutes), NgImageSliderModule,
+  ],
+  providers: [
+    AuthService,
+    UploadImageService,
+    ImageService
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
