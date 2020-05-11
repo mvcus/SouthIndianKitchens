62ee7846bf5a4229eb7382d40930d2d08b39511e
Chandan31g <chandangupta8702@gmail.com>
2020-05-11 21:29:57 +0530

    Revert "Commit ncluded code fixes"
    This reverts commit 259bdffed3ff18f8762f64a849005ed9912f10c3.

--- SouthIndianKitchens.APP/ClientApp/src/app/routes.ts
+++ SouthIndianKitchens.APP/ClientApp/src/app/routes.ts
@@ -6,9 +6,8 @@ import { AuthGuard } from './_gaurds/auth.guard';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { EpisodeComponent } from './episode/episode.component';
-import { RecpieComponent } from './recpie/recpie.component';
-import { ReviewsComponent } from './reviews/reviews.compnent';
-import { AboutusComponent } from './aboutus/aboutus.component';
+import { ImageSliderComponent } from './imageslider/imageslider.component';
+
 
 export const appRoutes: Routes = [
   
@@ -25,9 +24,7 @@ export const appRoutes: Routes = [
         { path: 'login', component: LoginComponent },
         { path: 'register', component: RegisterComponent },
         { path: 'episode', component: EpisodeComponent },
-        { path: 'recpie', component: RecpieComponent },
-        { path: 'reviews', component: ReviewsComponent },
-        { path: 'aboutus', component: AboutusComponent }
+        { path: 'imageslider', component: ImageSliderComponent },
       ]
   },
   { path: '**', redirectTo: '', pathMatch: 'full' }
