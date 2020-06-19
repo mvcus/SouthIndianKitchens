0dd8926c4cc0f3dc3622ce8a96511bbb410568ab
Kalaiarashi Pachiyappan <kalaimca2013@outlook.com>
2020-06-19 22:09:16 +0530

    Social Media Links drived from db

--- SouthIndianKitchens.APP/ClientApp/src/app/footer/footer.component.ts
+++ SouthIndianKitchens.APP/ClientApp/src/app/footer/footer.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { socialMediaLinks } from '../_Interfaces/socialMediaLinks.model';
+import { HttpClient } from '@angular/common/http';
+import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-footer',
@@ -6,6 +10,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-  constructor() { }
-  ngOnInit() { }
+
+  public medialinks: socialMediaLinks;
+  socialMediaLinks: string[];
+
+  constructor(private http: HttpClient, private authservice: AuthService, private alertifyjs: AlertifyService) { }
+
+  ngOnInit() {
+    this.getSocialMediaLinks();
+  }
+
+  private getSocialMediaLinks = () => {
+    this.http.get(this.authservice.baseUrl + 'getSocialMediaLinks')
+      .subscribe(res => {
+        this.socialMediaLinks = res as string[];
+      });
+  }
 }
